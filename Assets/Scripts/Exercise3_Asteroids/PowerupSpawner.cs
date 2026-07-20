47cdf152dbec098607665c1124e89a29be41f0eb
nicholasmerritt-acc <nicholas.merritt@g.austincc.edu>
2026-07-19 21:43:01 -0500

    powerup spawning

---
+++ Assets/Scripts/Exercise3_Asteroids/PowerupSpawner.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class PowerupSpawner : MonoBehaviour
+{
+    [SerializeField] private float initialSpawnWaitTime = 3f;
+    [SerializeField] private float spawnRepeatInterval = 4f;
+    [SerializeField] private int maxPowerups = 3;
+    public GameObject[] PowerupPrefabs;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        InvokeRepeating(nameof(SpawnPowerup), initialSpawnWaitTime, spawnRepeatInterval);
+    }
+
+    /// <summary>
+    /// create a powerup within the bounds of the screen
+    /// </summary>
+    void SpawnPowerup()
+    {
+        if (Powerup.Count < maxPowerups)
+        {
+            Vector2 powerupPosition = new Vector2(Random.Range(ScreenBounds.ScreenLeft, ScreenBounds.ScreenRight), Random.Range(ScreenBounds.ScreenBottom, ScreenBounds.ScreenTop));
+            Instantiate(PowerupPrefabs[Random.Range(0, PowerupPrefabs.Length)], powerupPosition, Quaternion.identity);
+        }
+    }
+}
